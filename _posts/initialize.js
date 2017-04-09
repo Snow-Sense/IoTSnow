1b294a6005c02deba133791e5ee51192a67c4096
kaymarchant1 <n02630113@hawkmail.newpaltz.edu>
2017-04-09 19:51:31 -0400

    Add files via upload

---
+++ _posts/initialize.js
@@ -0,0 +1,23 @@
+window.onload = initialize;
+
+function initialize(){
+  showGraphWithFirebaseData();
+}
+
+function showGraphWithFirebaseData(){
+refSensorData = firebase.database().ref().child("graphData");
+// refSensorData = firebase.database().ref().child("March13Snow");
+
+refSensorData.on("value", function(snapshot){
+  var dataReceivedFromFirebase = snapshot.val();
+  var dataDrawn = [];
+
+for (var key in dataReceivedFromFirebase){
+dataDrawn.push({Xvalue: dataReceivedFromFirebase[key].timestamp, Yvalue: dataReceivedFromFirebase[key].Height});
+}
+d3.selectAll("svg > *").remove();
+
+  drawDataPlot(dataDrawn);
+
+});
+}
